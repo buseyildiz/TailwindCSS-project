04e51ec414e154b93f7a000fb2bf081eabf20ddf
Buse Yıldız <buse@Buse-MacBook-Air.local>
2023-04-01 19:05:21 +0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container:{
+      center:true,
+      screens:{
+        lg:'1140px',
+        xl:'1140px',
+        '2xl':'1140px'
+      },
+    },
+    extend: {
+      fontFamily:{
+        gemunu:['Gemunu Libre', 'sans-serif'],
+        open:['Open Sans','sans-serif']
+      },
+      colors:{
+        'my-red':'#BC1A53',
+        'my-melon':'#FFD369',
+        'my-gray':'#DDDDDD',
+        'my-white':'#F7F7F7'
+      },
+      spacing:{
+        128:'32rem',
+      }
+    },
   },
   plugins: [],
 }
